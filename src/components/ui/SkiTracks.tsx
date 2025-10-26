7903efc5254e4ecb8e894c84d54ce8da568d8ab5
Luke Henning-Smith <lukeusydwork@gmail.com>
2025-10-26 19:08:39 +1100

    Simplify SkiTracks animation and remove initial delay

--- src/components/ui/SkiTracks.tsx
+++ src/components/ui/SkiTracks.tsx
@@ -1,8 +1,7 @@
-const paths: string[] = [
-  "M 1050 200 C 1000 220 800 270 950 300 C 1000 310 920 450 850 500 C 800 550 920 600 900 670 C 890 700 790 750 1050 950",
-];
-
 export default function SkiTracks() {
+  const skiPath =
+    "M 1000 200 C 1000 220 800 270 950 300 C 1000 310 920 450 850 500 C 800 550 920 600 900 670 C 890 700 790 750 1050 950";
+
   return (
     <div className="ski-bg" aria-hidden="true">
       <svg
@@ -11,22 +10,10 @@ export default function SkiTracks() {
         xmlns="http://www.w3.org/2000/svg"
         className="ski-svg"
       >
-        {paths.map((d, i) => (
-          <g
-            className="track"
-            key={i}
-            // set a CSS variable so child paths can pick up the delay (animation-delay doesn't inherit)
-            style={{ ["--delay" as any]: `${i * 1.18}s` }}
-          >
-            <path className="track-line" d={d} />
-            <path
-              className="track-line track-line--offset"
-              d={d}
-              // make the offset line start slightly after its partner for a natural look
-              style={{ ["--delay" as any]: `${i * 1.19}s` }}
-            />
-          </g>
-        ))}
+        <g className="track">
+          <path className="track-line" d={skiPath} />
+          <path className="track-line track-line--offset" d={skiPath} />
+        </g>
       </svg>
     </div>
   );
