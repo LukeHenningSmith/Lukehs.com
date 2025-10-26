98f9b6706815301652217175138610fb3ab2e666
Luke Henning-Smith <lhs@Mac-4.modem>
2025-10-26 13:31:22 +1100

    Minor styling adjustment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,12 @@ function Header() {
     <header className="w-full text-foreground p-4">
       <div className="flex justify-between">
         <h1 className="text-xl font-semibold">Logo</h1>
-        <h1 className="text-lg font-semibold">Links</h1>
-        <ModeToggle />
+        <div className="flex items-center gap-4">
+          <h1 className="text-lg">Experience</h1>
+          <h1 className="text-lg">Education</h1>
+          <h1 className="text-lg">Projects</h1>
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
--- src/components/theme/ModeToggle.tsx
+++ src/components/theme/ModeToggle.tsx
@@ -21,8 +21,8 @@ export function ModeToggle() {
       }}
       className="cursor-pointer"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
+      <Sun className="h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
+      <Moon className="absolute h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
