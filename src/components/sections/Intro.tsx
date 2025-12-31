3427e9a101398298457d402fc98df7cb703122c2
Luke Henning-Smith <lukeusydwork@gmail.com>
2025-12-31 20:15:44 +1100

    Updated introduction section description

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -10,6 +10,7 @@ import {
 import { SkillsContainer } from "../utility/skills/SkillsContainer";
 import { Section } from "../utility/Section";
 import { FadeUp } from "../layout/FadeUp";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export function Intro({ animationOffset }: { animationOffset?: number }) {
   const skills: SkillItem[] = [
@@ -24,23 +25,45 @@ export function Intro({ animationOffset }: { animationOffset?: number }) {
       <FadeUp delay={animationOffset}>
         <div className="flex flex-col gap-4 text-base text-muted-foreground">
           <span>
-            Hi! I'm Luke, a software engineer from Sydney with 2 years
-            experience building fullstack web applications in the finance
-            industry. The technologies I am most experienced with are:
+            Hey I'm Luke, a software engineer and skiing fanatic from Sydney,
+            Australia{" "}
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <span>🇦🇺</span>
+              </TooltipTrigger>
+              <TooltipContent side="bottom" align="center">
+                Australia
+              </TooltipContent>
+            </Tooltip>
+            .
+          </span>
+
+          <span>
+            I have spent the past 2 years building business critical full-stack
+            web applications in the finance industry, along with a few exciting{" "}
+            <u
+              className="hover:text-primary cursor-pointer"
+              onClick={() => scrollToId("projects")}
+              title="Scroll to Projects section"
+            >
+              personal projects
+            </u>
+            . The technologies I am most experienced with are:
           </span>
 
           <SkillsContainer skills={skills} />
 
-          <span className="mt-2">
+          <span>
             <i>
-              I am also an avid skiier with a{" "}
+              I am also an avid skiier (and recent snowboarder) with a{" "}
               <u
                 className="hover:text-primary cursor-pointer"
                 onClick={() => scrollToId("skiing")}
+                title="Scroll to Skiing section"
               >
                 bucket list
               </u>{" "}
-              to ski all over the world.
+              to ski all over the world ⛷️.
             </i>
           </span>
         </div>
