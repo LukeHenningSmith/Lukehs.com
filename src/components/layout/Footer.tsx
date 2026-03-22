95d33c7e3c04a3e36fb53ea5043722f6097818d4
Luke Henning-Smith <lukeusydwork@gmail.com>
2026-03-22 15:34:06 +1100

    Updated footer to 2026

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer({ animationOffset }: { animationOffset?: number }) {
     <FadeUp delay={animationOffset}>
       <footer className="w-full py-10 flex justify-center">
         <span className="text-sm text-muted-foreground">
-          © 2025 Luke Henning-Smith
+          © 2026 Luke Henning-Smith
         </span>
       </footer>
     </FadeUp>
